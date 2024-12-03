decab81bacb68a1a8c2c320fbe9e76f5e52eb39d
sebcroker <seb.h.croker@gmail.com>
2024-12-04 06:53:19 +1100

    New exercise in leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+/**Leap years are years divisible by four, but not divisible by 100 unless they are also divisible by 400. */
 
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+        return true;
+    } else if (year % 4 === 0 && year % 100 !== 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
